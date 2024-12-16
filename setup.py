8d9b8bb6536c67aba879d01a190161032881a23a
Sahil Lihas <sahillihas@Sahils-MacBook-Pro.local>
2024-12-16 12:46:56 +0530

    Bump version: 0.0.7 → 0.0.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="chunkator",
-    version='0.0.7',
+    version='0.0.8',
     description="A library for splitting text into sentences",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
